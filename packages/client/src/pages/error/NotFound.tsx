fe478260857ae609ff461896fc69fbc202a40170
Илья <cra7yy@gmail.com>
2026-02-21 16:17:20 +0300

    removed the unnecessary

--- packages/client/src/pages/error/NotFound.tsx
+++ packages/client/src/pages/error/NotFound.tsx
@@ -1,10 +1,7 @@
 import styles from './styles.module.scss';
-import { usePage } from '../../hooks/usePage';
 import { Link } from 'react-router-dom'
 
 export const NotFoundPage = () => {
-  usePage({ initPage: initNotFoundPage });
-
   return (
     <div className={ styles.container }>
       <Link to="/" className={ styles.link }>Перейти на главную страницу</Link>
