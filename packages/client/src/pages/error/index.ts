b02c8fc0b8c40fb8198553fbaf1915e338d56098
Alex6486 <achernov@bellintegrator.com>
2026-02-22 17:06:15 +0300

    authorization page

--- packages/client/src/pages/error/index.ts
+++ packages/client/src/pages/error/index.ts
@@ -1,1 +1,2 @@
 export { NotFoundPage, initNotFoundPage } from './NotFound';
+export { Error500 } from './Error500';
--- packages/client/src/pages/index.ts
+++ packages/client/src/pages/index.ts
@@ -2,4 +2,4 @@ export { ProfilePage } from './profile/ProfilePage';
 export { PasswordChange } from './profile/PasswordChange';
 export { AuthorizationPage } from './authorization';
 export { RegistrationPage } from './registration';
-export { NotFoundPage, initNotFoundPage } from './error';
+export { NotFoundPage, initNotFoundPage, Error500 } from './error';
