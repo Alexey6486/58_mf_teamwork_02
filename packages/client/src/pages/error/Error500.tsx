d2af6f2069fcc2d7011c070b6a03159f0894a215
Илья <cra7yy@gmail.com>
2026-02-23 11:46:24 +0300

    switched to tailwind

--- packages/client/src/pages/error/Error500.tsx
+++ packages/client/src/pages/error/Error500.tsx
@@ -1,9 +1,11 @@
-import styles from './styles.module.scss';
-
 export const Error500 = () => {
   return (
-    <div className={styles.container}>
-      <img className={styles.img} src={'src/assets/images/500.png'} alt="500" />
+    <div className="bg-main-light dark:bg-main-dark flex flex-col items-center min-h-screen p-5 box-border">
+      <img
+        className="max-w-full max-h-[70vh] w-auto h-auto object-contain"
+        src={'src/assets/images/500.png'}
+        alt="500"
+      />
     </div>
   );
 };
--- packages/client/src/pages/error/NotFound.tsx
+++ packages/client/src/pages/error/NotFound.tsx
@@ -1,13 +1,17 @@
-import styles from './styles.module.scss';
 import { Link } from 'react-router-dom';
+import { APP_TITLE_CLASS } from '../../constants/style-groups'
 
 export const NotFoundPage = () => {
   return (
-    <div className={styles.container}>
-      <Link to="/" className={styles.link}>
+    <div className="bg-main-light dark:bg-main-dark flex flex-col items-center min-h-screen p-5 box-border">
+      <Link to="/" className={ `${APP_TITLE_CLASS} mt-[100px] no-underline` }>
         Перейти на главную страницу
       </Link>
-      <img className={styles.img} src={'src/assets/images/404.png'} alt="404" />
+      <img
+        className="max-w-full max-h-[70vh] w-auto h-auto object-contain"
+        src={'src/assets/images/404.png'}
+        alt="404"
+      />
     </div>
   );
 };
