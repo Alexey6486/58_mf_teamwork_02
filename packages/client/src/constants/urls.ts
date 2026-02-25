4ad36652c2828467e8852ec3728ffd48a5052f74
Alex6486 <achernov@bellintegrator.com>
2026-02-25 19:38:54 +0300

    registration page

--- packages/client/src/constants/urls.ts
+++ packages/client/src/constants/urls.ts
@@ -2,6 +2,7 @@ export const URL_BASE = `https://ya-praktikum.tech/api/v2`;
 export const URL_BASE_IMG = `${URL_BASE}/resources`;
 export const URL_USER_DATA = `/auth/user`;
 export const URL_LOGIN = `/auth/signin`;
+export const URL_SIGNUP = `/auth/signup`;
 export const URL_LOGOUT = `/auth/logout`;
 export const URL_PROFILE = `/user/profile`;
 export const URL_AVATAR = `/user/profile/avatar`;
--- packages/client/src/types/index.ts
+++ packages/client/src/types/index.ts
@@ -1,4 +1,4 @@
 export type { StringSchemaValueType } from './yup-schema';
-export type { IUser, IUserPassword } from './user';
+export type { IUser, IUserPassword, IRegistrationForm } from './user';
 export type { TNullable } from './common';
 export type { IAuthorizationForm } from './authorization';
