ca1ceaf32538a10dbf61e2e5bb87e9c28f0940d7
Илья <cra7yy@gmail.com>
2026-02-28 13:09:08 +0300

    forum

---
+++ packages/client/src/pages/forum/data.ts
@@ -0,0 +1,8 @@
+import { type Topic } from '../../types/forum';
+
+export const initialTopics: Topic[] = [
+  { id: 1, title: 'А где игра' },
+  { id: 2, title: 'Объясните правила' },
+  { id: 3, title: 'Как считаются очки' },
+];
+
