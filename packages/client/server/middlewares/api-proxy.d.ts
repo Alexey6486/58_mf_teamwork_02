6b4005a536e9733650c8d98cf6fb9f4c09b7a859
Vadim Spivak <thevadimspivak@gmail.com>
2026-04-16 21:11:53 +0700

    fix after review

--- packages/client/server/middlewares/api-proxy.d.ts
+++
@@ -1,2 +0,0 @@
-/// <reference types="node" />
-export declare const apiProxy: import("http-proxy-middleware").RequestHandler<import("http").IncomingMessage, import("http").ServerResponse, (err?: any) => void>;
